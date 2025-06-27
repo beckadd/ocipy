74f30f70b1e8c3851910fcfb8ec4a52b6c711088
beckadd <bechtler.addison@gmail.com>
2025-06-27 16:16:16 -0400

    Refactor CLI and test configuration loader for improved readability

--- src/ocipy/cli.py
+++ src/ocipy/cli.py
@@ -1,29 +1,31 @@
 import typer
 
-cli = typer.Typer(
-    invoke_without_command=True
-)
+cli = typer.Typer(invoke_without_command=True)
+
 
 @cli.command()
 def add(packages: str):
     """Adds the specified package to the current project's dependencies."""
     ...
 
+
 @cli.command()
 def remove(packages: str):
     """Removes the specified package from the current project's dependencies."""
     ...
+
 
 @cli.command()
 def install(packages: str):
     """Installs the specified package into the current environment."""
     ...
 
+
 @cli.command()
 def uninstall(packages: str):
     """Uninstalls the specified package from the current environment."""
     ...
 
+
 @cli.command()
-def publish():
-    ...
+def publish(): ...
